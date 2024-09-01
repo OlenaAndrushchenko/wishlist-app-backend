282454eb157d01d48048b5fc4a345c60fe242150
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-01 05:35:44 +0200

    base project setup

---
+++ src/main/java/dev/olena/wishapp/WishappApplication.java
@@ -0,0 +1,13 @@
+package dev.olena.wishapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/olena/wishapp/WishappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.olena.wishapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
