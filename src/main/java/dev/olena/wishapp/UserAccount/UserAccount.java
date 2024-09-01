d93bb9adbe9ff097835277b70c2a66e5aafd0dbd
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-01 07:18:15 +0200

    base user account model and repository

---
+++ src/main/java/dev/olena/wishapp/UserAccount/UserAccount.java
@@ -0,0 +1,30 @@
+package dev.olena.wishapp.UserAccount;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_account")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserAccount {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
