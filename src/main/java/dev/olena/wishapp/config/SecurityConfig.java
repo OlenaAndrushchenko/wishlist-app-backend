cf47e7398b993b6a66ec1479249dcf31ecd9bb02
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-18 07:22:49 +0200

    basic security config

---
+++ src/main/java/dev/olena/wishapp/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package dev.olena.wishapp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Value("${api-endpoint}")
+    String apiEndpoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, apiEndpoint + "/register").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> httpBasic.realmName("WishappRealm"));
+        return http.build();
+    }
+}
