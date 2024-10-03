fefb6abab1f8f9569b3f78d11e072711b9d06d69
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-10-03 19:31:31 +0200

    updata data.sql with the new user fields; delete profile temporary

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,11 +2,10 @@
 CREATE TABLE IF NOT EXISTS user_account (
     user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    user_identifier VARCHAR(255) NOT NULL UNIQUE
 );
-
--- Insert initial data, handling potential duplicates to avoid errors on app restart.
-INSERT INTO user_account (username, password) VALUES ('user1', 'password1')
-ON DUPLICATE KEY UPDATE username = VALUES(username), password = VALUES(password);
-INSERT INTO user_account (username, password) VALUES ('user2', 'password2')
-ON DUPLICATE KEY UPDATE username = VALUES(username), password = VALUES(password);
+-- -- Insert initial data, handling potential duplicates to avoid errors on app restart.
+INSERT INTO user_account (username, password, email, user_identifier) VALUES ('user1', '$2a$12$goV6ZPdKrYzKTBXU02r.9.qUS.aSbtw.kGLK8S7z4gkF/4ygGu076', 'user1@email.com', 'uid12345')
+ON DUPLICATE KEY UPDATE username = VALUES(username), password = VALUES(password), email = VALUES(email), user_identifier = VALUES(user_identifier);
