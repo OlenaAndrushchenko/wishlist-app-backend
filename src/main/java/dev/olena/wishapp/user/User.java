bc79454acc89692404f5ea25d7031369ba7bc07c
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-10-03 02:44:25 +0200

    User modifications for login with email, new method to get username and user identifier for shared wishlist

--- src/main/java/dev/olena/wishapp/user/User.java
+++ src/main/java/dev/olena/wishapp/user/User.java
@@ -1,6 +1,7 @@
 package dev.olena.wishapp.user;
 
 import java.util.Collections;
+import java.util.UUID;
 
 import org.springframework.security.core.GrantedAuthority;
 import java.util.Collection;
@@ -15,6 +16,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -41,6 +43,9 @@ public class User implements UserDetails {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false, unique = true)
+    private String userIdentifier;
+
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Profile profile;
 
@@ -48,6 +53,24 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    public String getRealUsername() {
+        return this.username;
+    }
+
+    @PrePersist
+    private void generateUserIdentifier() {
+        this.userIdentifier = generateShortUUID();
+    }
+
+    private String generateShortUUID() {
+        return UUID.randomUUID().toString().substring(0, 8);
     }
 
     @Override
